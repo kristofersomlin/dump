02ec5c868cb2e77f342a5c2840e61c3a25aaa972
kristofersomlin <kristofer.somlin@gmail.com>
2017-11-29 13:33:22 +0100

    new files

---
+++ cplusplus/kap2/totaltpris.c
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int dag_antal,dagpris;
+  cout << "antal dagar? ";
+  cin >> dag_antal;
+  cout << "pris per dag? ";
+  cin >> dagpris;
+  cout << "totalt pris "
+       << dag_antal * dagpris << end1;
+}
